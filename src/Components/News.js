dbeca40ea0e2685304d5f6ef7173bba312579f43
DaveBhavesh <144877250+DaveBhavesh@users.noreply.github.com>
2023-10-04 12:04:36 +0530

    added favicon and fetch API for news in our App

--- src/Components/News.js
+++ src/Components/News.js
@@ -3,83 +3,42 @@ import NewsItem from './NewsItem'
 
 export class News extends Component {
 
-    articles=[
-      {
-        "source": { "id": "bbc-sport", "name": "BBC Sport" },
-        "author": null,
-        "title": "'Cricket can become the most inclusive sport'",
-        "description": "Following the publication of the ECB's response to the ICEC report, former England international and broadcaster Ebony Rainford-Brent says cricket can become the most inclusive sport in the country.",
-        "url": "http://www.bbc.co.uk/sport/cricket/66902424",
-        "urlToImage": "https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/5B44/production/_131246332_ebonyrainford-brent.jpg",
-        "publishedAt": "2023-09-25T21:22:22.3596901Z",
-        "content": "The ACE Programme was launched by Surrey County Cricket Club in 2020\r\nEbony Rainford-Brent was the first black woman to play cricket for England and is now a presenter and pundit for BBC Sport. She f… [+6203 chars]"
-      },
-      {
-        "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-        "author": null,
-        "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-        "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-        "publishedAt": "2020-04-27T11:41:47Z",
-        "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-      },
-      {
-        "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-        "author": null,
-        "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-        "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-        "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-        "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-        "publishedAt": "2020-03-30T15:26:05Z",
-        "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-      }
-    ]  
 
    constructor(){
       super();
-     console.log("Hell I am from News.js");
+     console.log("Hello I am from News.js");
      this.state={
-      articles:this.articles,
+      articles:[],
       loading: false
      }
-     }
+  }
+
+    async componentDidMount(){
+      let url="https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=01aad7c3543a49c2b4b9ee1a4a14339f"
+      let data= await fetch(url);
+      let parsedData=await data.json()
+      console.log(parsedData);
+      this.setState({articles:parsedData.articles})
+    } 
+
 
   render() {
     return (
       
-      <div className=' container my-3'>
+      <div className='container my-3' style={{backgroundColor:'#816F6F '}}>
       
-      <h2>News Bites Top Headlines</h2>
+      <h1 className='mb-5' style={{color:'white'}}>News Bites Top Headlines</h1>
+      <div className="row">
+      {this.state.articles.map((element)=>{
+        return(
+         <div className="col-md-4  mb-5" key={element.url}>
+        <NewsItem title={ element.title?element.title.slice(0,40):""} description={element.description?element.description.slice(0,90):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+        </div>)
+  })}
         
-        <div className='row my-5'>
-      
-            <div className="col-md-4  mb-5">
-                <NewsItem title="News" description="Will Update soon" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"/>
-            </div>
-
-            <div className="col-md-4 mb-5">
-                <NewsItem title="News" description="Will Update soon"/>
-            </div>
-
-            <div className="col-md-4 mb-5">
-                <NewsItem title="News" description="Will Update soon"/>
-            </div>
-
-            <div className="col-md-4 mb-5">
-                <NewsItem title="News" description="Will Update soon"/>
-            </div>
-
-            <div className="col-md-4 mb-5">
-                <NewsItem title="News" description="Will Update soon"/>
-            </div>
-
-            <div className="col-md-4 mb-5">
-                <NewsItem title="News" description="Will Update soon"/>
-            </div>
-
         </div>
-      </div>
+        </div>
+     
     )
   }
 }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-  
   render() {
-    let {title,description,imageUrl}=this.props;
+    let { title, description, imageUrl, newsUrl } = this.props;
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-  <img src={imageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/newsitem" className="btn  btn-sm btn-primary">Read More 📚</a>
-  </div>
-</div>
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={imageUrl?imageUrl:"https://www.dsij.in/Portals/0/EasyDNNnews/33605/image_280.jpg"} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a  href={newsUrl} target="_blank" className="btn  btn-sm btn-primary" >
+              Read More 📚
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
